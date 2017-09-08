26354cead4c43ee607f81a33658e81c422b12ac2
David Barrientos <david.barrientos.f@gmail.com>
2017-09-08 15:12:57 -0300

    sixth commit

---
+++ app/controllers/catches_controller.rb
@@ -0,0 +1,22 @@
+class CatchesController < ApplicationController
+  def index
+    @user = current_user
+    @catches = @user.catches
+  end
+
+  def add_pokemon
+    @pokemon = Pokemon.find(params[:pokemon_id])
+    @user = current_user
+    @user.pokemons << @pokemon
+    @user.save
+    redirect_to pokemons_path, notice: "El Pokemon ha sido agregado a su lista"
+  end
+
+  def level_up
+    @catch = Catch.find(params[:id])
+    @catch.level = 1 if @catch.level.nil?
+    @catch.level+=1
+    @catch.save
+    redirect_to catches_path, notice: "El Pokemon ha subido un nivel"
+  end
+end
